2148e0a629717cb59d0751aaae53bbeda981a879
Joubert Barcelos Costa Junior <joubertjun@yahoo.com.br>
2016-05-02 17:44:11 -0300

    Carga Inicial do Projeto

---
+++ operacoes.js
@@ -0,0 +1,20 @@
+function soma(a, b){
+	var soma = a + b;
+	return a + " + " + b + " = " + soma;  
+};
+
+function subtracao(a, b){
+	var subtracao = a - b;
+	return a + " + " + b + " = " + subtracao;  
+};
+
+function multiplicacao(a, b){
+	var multiplicacao = a * b;
+	return a + " + " + b + " = " + multiplicacao;  
+};
+
+function divisao(a, b){
+	var divisao = a / b;
+	return a + " + " + b + " = " + divisao;  
+};
+
